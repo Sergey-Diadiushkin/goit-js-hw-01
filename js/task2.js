64b694f5856c6da1ad21b667a7852ae1aa79f314
Sergey-Diadiushkin <sergeydiadiushkin@gmail.com>
2020-12-02 00:07:02 +0200

    task2

--- js/task2.js
+++ js/task2.js
@@ -1,2 +1,8 @@
 const total = 100;
-const ordered = 50;
+const ordered = 130;
+
+if (ordered > total) {
+  console.log("На складе недостаточно твоаров!");
+} else {
+  console.log("Заказ оформлен, с вами свяжется менеджер");
+}
