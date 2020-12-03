e2918a836b26d1c8276ba0bfd868089e9900579c
Sergey-Diadiushkin <sergeydiadiushkin@gmail.com>
2020-12-03 02:38:40 +0200

    task5

--- js/task5.js
+++ js/task5.js
@@ -1,6 +1,33 @@
-const deliveryToTheCountry = prompt("Доставка в какую страну?");
-const Китай = 100 кредитов;
-const Чили = 250 кредитов;
-const Австралия = 170 кредитов;
-const Индия = 80 кредитов;
-const Ямайка = 120 кредитов;
+const deliveryCountry = prompt("Доставка в какую страну?").toLowerCase();
+let Country1 = "китай";
+let Country2 = "чили";
+let Country3 = "австралия";
+let Country4 = "индия";
+let Country5 = "ямайка";
+let price;
+
+switch (deliveryCountry) {
+  case Country1:
+    price = 100;
+    break;
+
+  case Country2:
+    price = 250;
+    break;
+
+  case Country3:
+    price = 170;
+    break;
+
+  case Country4:
+    price = 80;
+    break;
+
+  case Country5:
+    price = 120;
+    break;
+
+  default:
+    alert("В вашей стране доставка не доступна");
+}
+alert(`Доставка в ${deliveryCountry} будет стоить ${price} кредитов`);
